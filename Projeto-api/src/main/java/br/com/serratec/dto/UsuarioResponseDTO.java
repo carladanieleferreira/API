e1a6638ba427028b21707923845b607040960c77
Carla Daniele Ferreira <daniele.fersan@gmail.com>
2024-10-15 20:26:51 -0300

    Projeto API

---
+++ Projeto-api/src/main/java/br/com/serratec/dto/UsuarioResponseDTO.java
@@ -0,0 +1,32 @@
+package br.com.serratec.dto;
+
+import br.com.serratec.entity.Usuario;
+
+public class UsuarioResponseDTO {
+	private String nome;
+	private String email;
+	
+	public UsuarioResponseDTO(Usuario usuario) {
+		super();
+		this.nome = usuario.getNome();
+		this.email = usuario.getEmail();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
